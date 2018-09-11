4ed181857b57db7175a5716a7523f0b6db671c1e
ale <ale-juarez@hotmail.ca>
2018-09-11 18:42:14 -0400

    with errors

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [
+  {
+    name: 'Ale',
+    photo: '',
+    scores: [5, 1, 4, 5, 4, 3, 4, 3, 2, 1]
+  },
+  {
+    name: 'Beatrice',
+    photo: '',
+    scores: [3, 1, 4, 5, 4, 3, 4, 3, 2, 1]
+  },
+  {
+    name: 'Carmen',
+    photo: '',
+    scores: [1, 1, 4, 5, 4, 3, 4, 3, 2, 1]
+  }
+];
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,6 @@
+var friends = require('../data/friends.js');
+module.exports = function(app) {
+  app.get('/app/friends', function(req, res) {
+    res.json(friends);
+  });
+};
